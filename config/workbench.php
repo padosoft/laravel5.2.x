dcacdc39d42649e199904e84157cc2026e5d7674
alevento <a@a.it>
2016-05-23 13:01:48 +0200

    aggiornati le ancore per sostituzione

--- config/workbench.php
+++ config/workbench.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'action'=> env(
+    'action' => env(
         'WORKBENCH_ACTION',
-        'cre'
+        'create'
     ),
     'type' => env(
         'WORKBENCH_TYPE',
@@ -10,7 +10,7 @@ return [
     ),
     'dir' => env(
         'WORKBENCH_DIR',
-        'Y:/public/'
+        ''
     ),
     'attemps' => env( 'WORKBENCH_ATTEMPS',
         '5'
@@ -36,4 +36,17 @@ return [
         'user' => env('WORKBENCH_SSH_USER', ''),
         'password' => env('WORKBENCH_SSH_SERVER', ''),
     ],
- ];
+    'type_repository' => [
+        'laravel' => env('WORKBENCH_TYPE_REPOSITORY_LARAVEL', 'laravel5.2.x-skeleton'),
+        'normal' => env('WORKBENCH_TYPE_REPOSITORY_NORMAL', ''),
+        'laravel_package' => env('WORKBENCH_TYPE_REPOSITORY_LARAVEL_PACKAGE', 'laravel5.2.x-package-skeleton'),
+        'agnostic_package' => env('WORKBENCH_TYPE_REPOSITORY_AGNOSTIC_PACKAGE', 'package-skeleton'),
+    ],
+    'substitute' => [
+        'author' =>env('WORKBENCH_SUBSTITUTION_AUTHOR', 'Padosoft'),
+        'emailauthor' =>env('WORKBENCH_SUBSTITUTION_EMAILAUTHOR', 'helpdesk@padosoft.com'),
+        'siteauthor' =>env('WORKBENCH_SUBSTITUTION_SITEAUTHOR', 'www.padosoft.com'),
+        'vendor' =>env('WORKBENCH_SUBSTITUTION_VENDOR', 'Padosoft'),
+        'files' =>env('WORKBENCH_SUBSTITUTION_FILES', 'readme.md,changelog.md,license.md,travis.yml,composer.json,src/config/sedCommand.sh,src/config/sedCommandProvider.sh'),
+    ],
+];
